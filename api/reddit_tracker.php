45457fde6b7025127a99a4694fb42172467841e8
SharkBounty <55365718+SharkBounty@users.noreply.github.com>
2026-04-26 00:28:57 -0300

    redditTRACKER

--- api/reddit_tracker.php
+++ api/reddit_tracker.php
@@ -1,7 +1,7 @@
 <?php
 function trackRedditPageVisit($clickId = null) {
     $pixelId = "a2_iwlpev9kbqi2";
-    $accessToken = "SEU_ACCESS_TOKEN_AQUI"; // Pegue no portal de Ads do Reddit
+    $accessToken = "eyJhbGciOiJSUzI1NiIsImtpZCI6IlNIQTI1NjpzS3dsMnlsV0VtMjVmcXhwTU40cWY4MXE2OWFFdWFyMnpLMUdhVGxjdWNZIiwidHlwIjoiSldUIn0.eyJzdWIiOiJ1c2VyIiwiZXhwIjo0OTMyOTMzNzA2LjA2NTIxNSwiaWF0IjoxNzc3MTczNzA2LjA2NTIxNCwianRpIjoiWGNEWXc0QXZIVU1HUURIdzlWWHFUUGVtamktX25RIiwiY2lkIjoiMVExRU96VFBXbll2ZXJocHR2Z1dzUSIsImxpZCI6InQyXzJkM2p2cHh4emEiLCJhaWQiOiJ0Ml8yZDNqdnB4eHphIiwiYXQiOjUsImxjYSI6MTc3NzE3MzcwNTM4NCwic2NwIjoiZUp5S1ZrcE1LVTdPenl0TExTck96TThyVm9vRkJBQUFfXzlCRmdidSIsImZsbyI6MTAsImxsIjp0cnVlfQ.p_kXdl64IrfkqOnCSnDzHIKAQD5P7PXGbYzrWySFI49ZYeJrvFSFrQYftWvsKA7SmF5YFG1MmhP5Aq0KeIFh39TGYMuxtqAN2kLyJZM5Cemsiiw6_xsI8hLoHzORKDSNVGgk_MCyAsQe_nh7uBv0KraCMwiQq185ff0_GdAijn6X9RiVeVJZR3wvN9mYXUYKMiLjfa2iZMFP1UjiLL-pBoeHDZUEDAyBT4dUNyRt5HoRPktc7X8kXyZhclNMwPWLkAmrM15RlFyYoJaTcLpaTuAcQX6t6ltVRy9orUPiN2-nzR0HVzUXJJ7ShaoA6XPvX9kldN8RSD9fo_DabnNZoQ"; // Pegue no portal de Ads do Reddit
 
     $data = [
         "events" => [[

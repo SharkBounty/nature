f729f56feaa904506c5496d056df0adae8d5d0ec
SharkBounty <55365718+SharkBounty@users.noreply.github.com>
2026-04-26 01:02:20 -0300

    test3

--- api/recetas.php
+++ api/recetas.php
@@ -30,12 +30,12 @@
         </script>
     <!-- End of Taboola Pixel Code -->
      <!-- Reddit Pixel -->
-        <script>
+        <!-- <script>
         !function(w,d){if(!w.rdt){var p=w.rdt=function(){p.sendEvent?p.sendEvent.apply(p,arguments):p.callQueue.push(arguments)};p.callQueue=[];var t=d.createElement("script");t.src="https://www.redditstatic.com/ads/pixel.js?pixel_id=a2_iwlpev9kbqi2",t.async=!0;var s=d.getElementsByTagName("script")[0];s.parentNode.insertBefore(t,s)}}(window,document);rdt('init','a2_iwlpev9kbqi2');rdt('track', 'PageVisit');
-        // rdt('track', 'PageVisit', {
-        //     "externalId": "<?php echo $event_id; ?>"
-        // });
-        </script>
+        rdt('track', 'PageVisit', {
+            "externalId": "<?php echo $event_id; ?>"
+        });
+        </script> -->
     <!-- DO NOT MODIFY UNLESS TO REPLACE A USER IDENTIFIER -->
     <!-- End Reddit Pixel -->
     <meta charset="UTF-8">
